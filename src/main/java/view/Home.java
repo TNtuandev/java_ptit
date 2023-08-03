b84473d74e8b9f054712b115cc19e9b47afba45d
tuan tina <tuan.nguyen@desports.gg>
2023-08-04 00:06:16 +0700

    dd

--- src/main/java/view/Home.java
+++ src/main/java/view/Home.java
@@ -64,7 +64,6 @@ public class Home extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         txtTenTacGia = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        txtChuyenNganh = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         txtNamXb = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
@@ -74,6 +73,7 @@ public class Home extends javax.swing.JFrame {
         btnSave1 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
+        txtChuyenNganh = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -119,12 +119,6 @@ public class Home extends javax.swing.JFrame {
 
         jLabel2.setText("Tác giả");
 
-        txtChuyenNganh.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtChuyenNganhActionPerformed(evt);
-            }
-        });
-
         jLabel3.setText("Chuyên Ngành");
 
         txtNamXb.addActionListener(new java.awt.event.ActionListener() {
@@ -152,7 +146,20 @@ public class Home extends javax.swing.JFrame {
 
         btnSave1.setText("Xuất File");
 
-        initTable();
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null}
+            },
+            new String [] {
+                "Mã sách ", "Tên sách ", "Chuyên Ngành", "Năm xuất bản", "Tác giả", "Số lượng"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        txtChuyenNganh.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Khoa học tự nhiên", "Văn học – Nghệ thuật", "Điện tử Viễn thông", "Công nghệ thông tin" }));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -168,17 +175,17 @@ public class Home extends javax.swing.JFrame {
                             .addComponent(txtNamXb, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel3)
-                                .addGap(46, 46, 46)
-                                .addComponent(txtChuyenNganh, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel2)
-                                .addGap(46, 46, 46)
-                                .addComponent(txtTenTacGia, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jLabel1)
                                 .addGap(46, 46, 46)
-                                .addComponent(txtTenSach, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                .addComponent(txtTenSach, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING))
+                                .addGap(46, 46, 46)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(txtTenTacGia)
+                                    .addComponent(txtChuyenNganh, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(jPanel1Layout.createSequentialGroup()
                             .addComponent(jLabel5)
@@ -189,7 +196,7 @@ public class Home extends javax.swing.JFrame {
                             .addComponent(btnAdd1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                 .addGap(96, 96, 96)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 576, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(94, Short.MAX_VALUE))
+                .addContainerGap(64, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -206,8 +213,8 @@ public class Home extends javax.swing.JFrame {
                             .addComponent(jLabel2))
                         .addGap(29, 29, 29)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(txtChuyenNganh, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel3))
+                            .addComponent(jLabel3)
+                            .addComponent(txtChuyenNganh, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(32, 32, 32)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(txtNamXb, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -240,10 +247,6 @@ public class Home extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void txtChuyenNganhActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtChuyenNganhActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtChuyenNganhActionPerformed
-
     private void txtTenTacGiaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtTenTacGiaActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_txtTenTacGiaActionPerformed
@@ -257,7 +260,7 @@ public class Home extends javax.swing.JFrame {
         System.out.println("Add event");
         List<String> bookAtt = new ArrayList();
         bookAtt.add(txtTenSach.getText());
-        bookAtt.add(txtChuyenNganh.getText());
+        bookAtt.add(txtChuyenNganh.getItemAt(txtChuyenNganh.getSelectedIndex()));
         bookAtt.add(txtNamXb.getText());
         bookAtt.add(txtSoLuong.getText());
         bookAtt.add(txtTenTacGia.getText());
@@ -323,7 +326,7 @@ public class Home extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JTable jTable1;
-    private javax.swing.JTextField txtChuyenNganh;
+    private javax.swing.JComboBox<String> txtChuyenNganh;
     private javax.swing.JTextField txtNamXb;
     private javax.swing.JTextField txtSoLuong;
     private javax.swing.JTextField txtTenSach;
